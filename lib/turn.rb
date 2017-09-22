c97f2598090dec20bf07a7978609ca68bb2080ec
Aaron Samuel <aasamuelj@gmail.com>
2017-09-22 21:33:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def position_taken?(board, index)
-  board[index] == "X" || board[index] == "O"  
+  board[index] == "X" || board[index] == "O" ? true : false
 end
 
 def valid_move?(board, index)
